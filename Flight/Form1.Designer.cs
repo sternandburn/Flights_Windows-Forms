4307e5a5fef7735d37fa5f51fe784fb12969da45
sternandburn <51689230+sternandburn@users.noreply.github.com>
2022-04-21 23:15:02 -0500

    Add files via upload

---
+++ Flight/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace SternFlight
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbFrom = new System.Windows.Forms.ComboBox();
+            this.lbFlights = new System.Windows.Forms.ListBox();
+            this.cbTo = new System.Windows.Forms.ComboBox();
+            this.btnNSF = new System.Windows.Forms.Button();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.lblTO = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbFrom
+            // 
+            this.cbFrom.FormattingEnabled = true;
+            this.cbFrom.Location = new System.Drawing.Point(129, 87);
+            this.cbFrom.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbFrom.Name = "cbFrom";
+            this.cbFrom.Size = new System.Drawing.Size(222, 24);
+            this.cbFrom.TabIndex = 0;
+            this.cbFrom.SelectedIndexChanged += new System.EventHandler(this.cbFrom_SelectedIndexChanged);
+            // 
+            // lbFlights
+            // 
+            this.lbFlights.FormattingEnabled = true;
+            this.lbFlights.ItemHeight = 16;
+            this.lbFlights.Location = new System.Drawing.Point(129, 134);
+            this.lbFlights.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lbFlights.Name = "lbFlights";
+            this.lbFlights.Size = new System.Drawing.Size(743, 276);
+            this.lbFlights.TabIndex = 2;
+            this.lbFlights.SelectedIndexChanged += new System.EventHandler(this.lbFlights_SelectedIndexChanged);
+            // 
+            // cbTo
+            // 
+            this.cbTo.FormattingEnabled = true;
+            this.cbTo.Location = new System.Drawing.Point(639, 87);
+            this.cbTo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbTo.Name = "cbTo";
+            this.cbTo.Size = new System.Drawing.Size(233, 24);
+            this.cbTo.TabIndex = 6;
+            this.cbTo.SelectedIndexChanged += new System.EventHandler(this.cbTo_SelectedIndexChanged);
+            // 
+            // btnNSF
+            // 
+            this.btnNSF.BackColor = System.Drawing.SystemColors.Window;
+            this.btnNSF.Location = new System.Drawing.Point(387, 418);
+            this.btnNSF.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnNSF.Name = "btnNSF";
+            this.btnNSF.Size = new System.Drawing.Size(261, 28);
+            this.btnNSF.TabIndex = 7;
+            this.btnNSF.Text = "Show Flights";
+            this.btnNSF.UseVisualStyleBackColor = false;
+            this.btnNSF.Click += new System.EventHandler(this.btnNSF_Click);
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Location = new System.Drawing.Point(180, 68);
+            this.lblFrom.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(55, 17);
+            this.lblFrom.TabIndex = 9;
+            this.lblFrom.Text = "Depart:";
+            this.lblFrom.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblTO
+            // 
+            this.lblTO.AutoSize = true;
+            this.lblTO.Location = new System.Drawing.Point(776, 68);
+            this.lblTO.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTO.Name = "lblTO";
+            this.lblTO.Size = new System.Drawing.Size(49, 17);
+            this.lblTO.TabIndex = 10;
+            this.lblTO.Text = "Arrive:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnClear.Location = new System.Drawing.Point(387, 454);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(261, 28);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "Clear Flights";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblTO);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.btnNSF);
+            this.Controls.Add(this.cbTo);
+            this.Controls.Add(this.lbFlights);
+            this.Controls.Add(this.cbFrom);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbFrom;
+        private System.Windows.Forms.ListBox lbFlights;
+        private System.Windows.Forms.ComboBox cbTo;
+        private System.Windows.Forms.Button btnNSF;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.Label lblTO;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Flight/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SternFlight
+{
+    public partial class Form1 : Form
+    {
+        
+        string[] vertices = { "Seattle", "San Francisco", "Los Angeles", "Denver", "Kansas City", "Chicago", "Boston",
+                "New York", "Atlanta", "Miami", "Dallas", "Houston" };
+
+        // Edge array for graph
+        int[,] edges = { { 0, 1, 807 }, { 0, 3, 1331 }, { 0, 5, 2097 }, { 1, 0, 807 }, { 1, 2, 381 }, { 1, 3, 1267 },
+                { 2, 1, 381 }, { 2, 3, 1015 }, { 2, 4, 1663 }, { 2, 10, 1435 }, { 3, 0, 1331 }, { 3, 1, 1267 },
+                { 3, 2, 1015 }, { 3, 4, 599 }, { 3, 5, 1003 }, { 4, 2, 1663 }, { 4, 3, 599 }, { 4, 5, 533 },
+                { 4, 7, 1260 }, { 4, 8, 864 }, { 4, 10, 496 }, { 5, 0, 2097 }, { 5, 3, 1003 }, { 5, 4, 533 },
+                { 5, 6, 983 }, { 5, 7, 787 }, { 6, 5, 983 }, { 6, 7, 214 }, { 7, 4, 1260 }, { 7, 5, 787 },
+                { 7, 6, 214 }, { 7, 8, 888 }, { 8, 4, 864 }, { 8, 7, 888 }, { 8, 9, 661 }, { 8, 10, 781 },
+                { 8, 11, 810 }, { 9, 8, 661 }, { 9, 11, 1187 }, { 10, 2, 1435 }, { 10, 4, 496 }, { 10, 8, 781 },
+                { 10, 11, 239 }, { 11, 8, 810 }, { 11, 9, 1187 }, { 11, 10, 239 } };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            foreach(string city in vertices)
+            {
+                cbFrom.Items.Add(city);
+                cbTo.Items.Add(city);
+            }
+        }
+
+
+        private void cbTo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbFrom_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void lbFlights_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNSF_Click(object sender, EventArgs e)
+        {
+           
+            if(cbFrom.SelectedIndex >= 0 && cbTo.SelectedIndex >= 0)
+            {
+                    for (int i = 0; i < edges.GetLength(0); i++)
+                    {
+
+                        if (cbFrom.SelectedIndex == edges[i, 0] && cbTo.SelectedIndex == edges[i,1])
+                        {
+                            lbFlights.Items.Add("Miles: " + edges[i, 2] + " from " + vertices[edges[i, 0]] + " to " + vertices[edges[i, 1]]);
+                            
+                        }
+                    }
+                    
+            }
+                
+            
+             else if (cbFrom.SelectedIndex >= 0)
+                {
+                    for (int i = 0; i < edges.GetLength(0); i++)
+                    {
+
+                        if (cbFrom.SelectedIndex == edges[i, 0])
+                        {
+                            lbFlights.Items.Add("Total Miles: " + edges[i, 2] + " from " + vertices[edges[i, 0]] + " to " + vertices[edges[i, 1]]);
+                        }
+                    }
+                }
+            
+            else
+            {
+                MessageBox.Show("Please make a selection");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lbFlights.Items.Clear();
+        }
+    }
+}
